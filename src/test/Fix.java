22aa79d31d97b73c2611e0a834a95e0c023f09d2
SergeyB <sergiibugaienko@gmail.com>
2025-06-12 20:36:17 +0200

    fix add

---
+++ src/test/Fix.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 12.06.2025
+ */
+
+public class Fix {
+
+    @Override
+    public String toString() {
+        return "Fix branch";
+    }
+}
