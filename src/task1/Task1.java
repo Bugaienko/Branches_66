176d9692976f6032ae3be903892226b4145d8cf4
SergeyB <sergiibugaienko@gmail.com>
2025-06-13 18:53:10 +0200

    task1 done

---
+++ src/task1/Task1.java
@@ -0,0 +1,14 @@
+package task1;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 13.06.2025
+ */
+
+public class Task1 {
+
+    @Override
+    public String toString() {
+        return "Sergey";
+    }
+}
