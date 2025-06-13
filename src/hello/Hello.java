7897cb8444f2c85febf8a380f26ff06d0249e11f
SergeyB <sergiibugaienko@gmail.com>
2025-06-13 18:26:57 +0200

    create hello

---
+++ src/hello/Hello.java
@@ -0,0 +1,14 @@
+package hello;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 13.06.2025
+ */
+
+public class Hello {
+    @Override
+    public String toString() {
+
+        return "Hello";
+    }
+}
